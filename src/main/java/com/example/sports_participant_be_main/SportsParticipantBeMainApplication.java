f71418e5a4a864af4cff2cc29789b18ecd8076e6
Sviatoslav <sviatoslav.kytsara@artcodevision.com>
2022-09-10 00:01:55 +0300

    initial commit

---
+++ src/main/java/com/example/sports_participant_be_main/SportsParticipantBeMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.sports_participant_be_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsParticipantBeMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsParticipantBeMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sports_participant_be_main/SportsParticipantBeMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sports_participant_be_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsParticipantBeMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
