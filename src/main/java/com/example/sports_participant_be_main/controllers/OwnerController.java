ea4a8962de590f6cab2910eb45049219a8b7d1bc
fayonn <s.kytsara@gmail.com>
2022-09-10 19:14:48 +0300

    add custom exception handler

--- src/main/java/com/example/sports_participant_be_main/controllers/OwnerController.java
+++ src/main/java/com/example/sports_participant_be_main/controllers/OwnerController.java
@@ -3,6 +3,7 @@ package com.example.sports_participant_be_main.controllers;
 import com.example.sports_participant_be_main.models.Owner;
 import com.example.sports_participant_be_main.services.OwnerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -10,12 +11,14 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/owner")
 @AllArgsConstructor
+@Slf4j
 public class OwnerController {
 
     private final OwnerService ownerService;
 
     @PostMapping("/add")
     public void add(@Valid @RequestBody Owner owner){
+//        log.info("Test {}, really {}", 1, 4);
         ownerService.save(owner);
     }
 }
