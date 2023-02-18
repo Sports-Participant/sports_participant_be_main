b9df09f8f83ba39ed58fae2fb41e98e6c008daa5
fayonn <s.kytsara@gmail.com>
2023-02-18 23:12:44 +0200

    fix jwt request model

--- src/main/java/com/example/sports_participant_be_main/security/jwt/JwtRequest.java
+++ src/main/java/com/example/sports_participant_be_main/security/jwt/JwtRequest.java
@@ -2,9 +2,11 @@ package com.example.sports_participant_be_main.security.jwt;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequest {
 
     private String email;
