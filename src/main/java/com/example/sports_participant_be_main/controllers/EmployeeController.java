c86225151b000577946618803af61a1de2572903
fayonn <s.kytsara@gmail.com>
2023-03-11 02:17:34 +0200

    update employee controller, service , repo and dto, update role controller and service

--- src/main/java/com/example/sports_participant_be_main/controllers/EmployeeController.java
+++ src/main/java/com/example/sports_participant_be_main/controllers/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.sports_participant_be_main.controllers;
 
 import com.example.sports_participant_be_main.dto.EmployeeDto;
+import com.example.sports_participant_be_main.models.Employee;
 import com.example.sports_participant_be_main.services.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/gym_brands/{gym_brand_id}/employee")
+@RequestMapping("/gym_brands/{gym_brand_id}/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -22,7 +25,20 @@ public class EmployeeController {
             @Valid @RequestBody EmployeeDto employeeDto
     ) {
         return new ResponseEntity<>(
-                this.employeeService.save(employeeDto.ofEntity(), gymBrandId, employeeDto.getRole_ids()).ofDto(),
+                this.employeeService.save(employeeDto.ofEntity(), gymBrandId, employeeDto.getRoleIds()).ofDto(),
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<EmployeeDto>> getAll(
+            @PathVariable("gym_brand_id") UUID gymBrandId
+    ) {
+        return new ResponseEntity<>(
+                this.employeeService.getAllByGymBrandId(gymBrandId)
+                        .stream()
+                        .map(Employee::ofDto)
+                        .collect(Collectors.toSet()),
                 HttpStatus.CREATED
         );
     }
--- src/main/java/com/example/sports_participant_be_main/controllers/RoleController.java
+++ src/main/java/com/example/sports_participant_be_main/controllers/RoleController.java
@@ -10,25 +10,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.relation.RoleNotFoundException;
-import java.util.Set;
+import java.util.Collection;
+
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/users/{user_id}/roles")
 @AllArgsConstructor
 @Slf4j
 public class RoleController {
     private final RoleService roleService;
     private final UserService userService;
 
-    @GetMapping()
-    public ResponseEntity<Set<RoleDto>> getByUserId(@PathVariable("user_id") UUID userId) throws RoleNotFoundException {
-        Set<Role> roles = this.userService.getRolesByUserId(userId);
+    @GetMapping("/users/{user_id}/roles")
+    public ResponseEntity<Collection<RoleDto>> getByUserId(@PathVariable("user_id") UUID userId) throws RoleNotFoundException {
+        Collection<Role> roles = this.userService.getRolesByUserId(userId);
+
+        return new ResponseEntity<>(
+                roles.stream().map(Role::ofDto).collect(Collectors.toSet()),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<Collection<RoleDto>> getAll(){
+        Collection<Role> roles = this.roleService.getAll();
 
         return new ResponseEntity<>(
                 roles.stream().map(Role::ofDto).collect(Collectors.toSet()),
